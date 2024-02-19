3c3ec90dec96a9c8caa725e08c30808dccfa53bf
JoãoPaulo <144693332+jpougithub@users.noreply.github.com>
2024-02-19 20:18:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let nome = 'joao'
+console.log(nome)
+
+let total = 10+5*2/2+20;
+console.log(total)
+
+let soma = 'joao'+50;
+isNaN(soma)
+let calculo = 'manoel'+100;
+isNaN(calculo)
+
+let juntar = +'200'+50;
+
+console.log(juntar)
+
+let incremento = 5
+console.log(++incremento)
+
+let numero = '80'
+let unidade = 'kg'
+let peso = numero /2  + unidade
+console.log(peso)
+pesoPorDois = numero / 2 + unidade
+console.log(pesoPorDois)
+
+
+
